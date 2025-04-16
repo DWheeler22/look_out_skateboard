8982fb5b666500a0cba1817bf789df1d5e9426ee
DJ Wheeler <dxw334@psu.edu>
2025-04-15 21:05:30 -0400

    Added POST method for adding a new user to a JSON file containing users and their respective reservations.

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
-var express = require('express')
-var cors = require('cors')
-var app = express
-
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+const app = express()
 app.use(cors())
+
+const port = '3000'
+const userFile = 'userFile.json'
+const reservationFile = 'reservationFile.json'
+
+
+// Add username to the system
+app.post('/users/:userName', (req, res) => {
+    let userName = req.params.userName
+    fs.readFile(userFile, (err, data) =>{
+        if (err) throw err
+        let userData = JSON.parse(data)
+        let userObj = {name:userName, reservations:[]}
+        userData.users.push(userObj)
+        let json_out = JSON.stringify(userData)
+        fs.writeFile(userFile, json_out, (err) => {
+            if (err) throw err
+            console.log(`User ${userName} has been created.`)
+            res.send(`User ${userName} has been created.`)
+        })
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`)
+})
