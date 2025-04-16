b81418985815c58ee90d9326cee623cc0e4fa531
DJ Wheeler <dxw334@psu.edu>
2025-04-16 12:05:04 -0400

    Added functionality in user creation to instantiate a reservation array for them

--- server.js
+++ server.js
@@ -6,26 +6,33 @@ app.use(cors())
 
 const port = '3000'
 const userFile = 'users.json'
-const reservationFile = 'reservationFile.json'
+const reservFile = 'reservations.json'
 
 // Instantiate JSON files
 let emptyUserObj = {"users":[]}
 let emptyReservationObj = {}
 fs.writeFileSync(userFile, JSON.stringify(emptyUserObj))
-fs.writeFileSync(reservationFile, JSON.stringify(emptyReservationObj))
+fs.writeFileSync(reservFile, JSON.stringify(emptyReservationObj))
 
 // Add username to the system
 app.post('/users/:userName', (req, res) => {
     let userName = req.params.userName
     let userStr = fs.readFileSync(userFile)
-    let userData = JSON.parse(userStr)
-    let userObj = {name:userName, reservations:[]}
-    userData.users.push(userObj)
-    
-    fs.writeFileSync(userFile, JSON.stringify(userData))
+    let userJSON = JSON.parse(userStr)
+    let newUserObj = {name:userName, reservations:[]}
+    userJSON.users.push(newUserObj)
+    fs.writeFileSync(userFile, JSON.stringify(userJSON))
+
+    let reservStr = fs.readFileSync(reservFile)
+    let reservJSON = JSON.parse(reservStr)
+    reservJSON[userName] = []
+    fs.writeFileSync(reservFile, JSON.stringify(reservJSON))
+
     console.log(`User ${userName} has been created.`)
     res.send(`User ${userName} has been created.`)
     })
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
